55a4c36fdb598a9636c31555b81f3a3d5aea0fb7
azkalonz <aeartixenter@gmail.com>
2024-04-16 08:53:40 +0800

    Clean up

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -4,30 +4,24 @@ use App\Http\Controllers\VerifyEmailController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// Authentication
 Route::post('/login', 'App\Http\Controllers\UserController@login');
 
+// Users
 Route::prefix('/users')->middleware('auth:api')->group(function () {
     Route::post('/', 'App\Http\Controllers\UserController@register')->middleware('scope:create-users');
-
     Route::put('/{id}', 'App\Http\Controllers\UserController@updateClient')->middleware('scope:update-users');
-
     Route::get('/', 'App\Http\Controllers\UserController@listUsers')->middleware('scope:view-all-users');
-
     Route::get('/{id}', 'App\Http\Controllers\UserController@getUser')->middleware('scope:view-users');
-
     Route::delete('/{id}', 'App\Http\Controllers\UserController@deleteUser')->middleware('scope:delete-users');
 });
 
-
+// Clients
 Route::prefix('clients')->middleware('auth:api')->group(function () {
     Route::post('/', 'App\Http\Controllers\ClientController@createClient')->middleware('scope:create-clients');
-
     Route::put('/{id}', 'App\Http\Controllers\ClientController@updateClient')->middleware('scope:update-clients');
-
     Route::get('/', 'App\Http\Controllers\ClientController@listClients')->middleware('scope:view-all-clients');
-
     Route::get('/{id}', 'App\Http\Controllers\ClientController@getClient')->middleware('scope:view-clients');
-
     Route::delete('/{id}', 'App\Http\Controllers\ClientController@deleteClient')->middleware('scope:delete-clients');
 });
 
